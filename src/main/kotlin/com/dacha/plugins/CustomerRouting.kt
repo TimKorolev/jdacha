dc9a6cdf777149efe5bc75239a364a695ca4edef
Timofei Korolev <timofei.korolev@jetbrains.com>
2021-09-19 21:03:38 +0300

    added exodus

--- src/main/kotlin/com/dacha/plugins/CustomerRouting.kt
+++ src/main/kotlin/com/dacha/plugins/CustomerRouting.kt
@@ -1,50 +1,40 @@
 package com.dacha.plugins
 
 import com.dacha.models.Customer
-import com.dacha.models.customerStorage
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import jetbrains.exodus.entitystore.*
 
 fun Route.customerRouting() {
     route("/customer") {
         get {
-            if (customerStorage.isNotEmpty()) {
-                call.respond(customerStorage)
-            } else {
-                call.respondText("No customers found", status = HttpStatusCode.NotFound)
-            }
-        }
-        get("{id}") {
-            val id = call.parameters["id"] ?: return@get call.respondText(
-                "Missing or malformed id",
-                status = HttpStatusCode.BadRequest
-            )
-            val customer =
-                customerStorage.find { it.id == id } ?: return@get call.respondText(
-                    "No customer with id $id",
-                    status = HttpStatusCode.NotFound
-                )
+            var customer = ""
+            PersistentEntityStores.newInstance("/Users/Timofei.Korolev/IdeaProjects/dacha/database").use { entityStore ->
+                    entityStore.executeInTransaction(
+                        StoreTransactionalExecutable { txn: StoreTransaction ->
+                            txn.getAll("Customer").forEach { entity ->
+                                customer += entity.getProperty("id")
+                            }
+                        })
+                }
             call.respond(customer)
         }
         post {
             val customer = call.receive<Customer>()
-            customerStorage.add(customer)
-            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
-        }
-        delete("{id}") {
-            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
-            if (customerStorage.removeIf { it.id == id }) {
-                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
-            } else {
-                call.respondText("Not Found", status = HttpStatusCode.NotFound)
+            PersistentEntityStores.newInstance("/Users/Timofei.Korolev/IdeaProjects/dacha/database").use { entityStore ->
+                entityStore.executeInTransaction(
+                    StoreTransactionalExecutable { txn: StoreTransaction ->
+                        val customerEntity: Entity = txn.newEntity("Customer")
+                        customerEntity.setProperty("id", customer.id)
+                    })
             }
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
         }
     }
 }
-
 
 fun Application.registerCustomerRoutes() {
     routing {
